a2c61307f0992154ef9d307641b4126a32fe1a60
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-07-07 13:57:27 -0300

    models de conta e categoria

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Conta, Categoria
 
-# Register your models here.
+@admin.register(Conta)
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ['apelido','banco','tipo','valor']
+    
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['categoria','essencial','valor_planejamento']
--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from . models import Conta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def home(request):
     return render(request, 'home.html')
     
+
+def gerenciar(request):
+    contas = Conta.objects.all()
+    total_conta = 0
+    for conta in contas:
+        total_conta += conta.valor
+    return render(request, 'gerenciar.html', {'contas': contas,'total_conta':total_conta,})
+
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('gerenciar')
+    
+    conta = Conta(
+        apelido= apelido,
+        banco= banco,
+        tipo = tipo,
+        valor= valor,
+        icone= icone
+    )
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta Cadastrada com sucesso!')
+    return redirect('gerenciar')
+
+def deletar_banco(request, id):
+    conta = get_object_or_404(Conta, pk=id)
+    conta.delete()
+    messages.add_message(request, constants.SUCCESS, 'Conta removida com sucesso!') 
+    return redirect('gerenciar')
